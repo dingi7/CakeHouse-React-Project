31c8a2fc341cc95aa8792d3cf31929654ffae51c
dingi7 <kamenkanev88@gmail.com>
2023-03-25 01:02:27 +0200

    Added new paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { HomePage } from './components/Pages/Home';
 import { Header } from './components/Partials/Header';
 import { ShopPage } from './components/Pages/Shop';
 import { SingleProductPage } from './components/Pages/SingleProduct';
+import { LoginPage } from './components/Pages/Login';
+import { RegisterPage } from './components/Pages/Register';
+import { ShoppingCartPage } from './components/Pages/ShoppingCart';
 
 function App() {
     return (
@@ -14,6 +17,9 @@ function App() {
                     <Route path="/" element={<HomePage/>} />
                     <Route path="/shop" element={<ShopPage/>} />
                     <Route path="/shop/:id" element={<SingleProductPage/>} />
+                    <Route path="/login" element={<LoginPage/>} />
+                    <Route path="/register" element={<RegisterPage/>} />
+                    <Route path="/cart" element={<ShoppingCartPage/>} />
                 </Routes>
             </div>
         </BrowserRouter>
